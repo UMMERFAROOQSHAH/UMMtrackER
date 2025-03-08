2f4a7087446a01300d4545c6afaadd60d40614b7
UMMER FAROOQ SHAH <suhaanumer07@gmail.com>
2025-03-08 21:25:16 +0530

    Update UMMtrackER.py

--- UMMtrackER.py
+++ UMMtrackER.py
@@ -1,16 +1,30 @@
 from tkinter import * 
 from PIL import Image,ImageTk #FOR SETTING IMAGES.
-from tkinter import ttk
+from tkinter import ttk  #FOR MAKING WIDGES(COMBO-BOXES HERE)
 import mysql.connector
-from tkinter import messagebox
+from tkinter import messagebox          #POPUP MESSAGE
 class ummtracker:
     def __init__(self,root):#WINDOW NAME
         self.root=root
-        self.root.title("UMMtrackER") #TITLE OF MANAGEMENT SYSTEM
+        self.root.title("UMMER FAROOQ SHAH") #TITLE OF MANAGEMENT SYSTEM
         self.root.geometry("1550x800+0+0") #SIZE OF WINDOW I.E WIDTH and HEIGHT,0,0 =ORIGIN FROM WHERE SCREEN SHOULD START.
         #***************************ADDING VARIABLES****************************
         self.addmedvar=StringVar()
         self.addrefvar=StringVar()
+        self.refvar=StringVar()
+        self.cpynamevar=StringVar()
+        self.typevar=StringVar()
+        self.mednamevar=StringVar()
+        self.lotvar=StringVar()
+        self.issuevar=StringVar()
+        self.expirevar=StringVar()
+        self.usesvar=StringVar()
+        self.efectsvar=StringVar()
+        self.warningvar=StringVar()
+        self.dosagevar=StringVar()
+        self.pricevar=StringVar()
+        self.qntyvar=StringVar()
+        
 
         
              #MAKING OF TITLE AND POSITIONING IT:-
@@ -44,25 +58,25 @@ class ummtracker:
         buttonframe=Frame(self.root,bd=15,relief=RIDGE,padx=20)
         buttonframe.place(x=0,y=470,width=1265,height=65)
             #MAKING MAIN BUTTONS:-
-        button=Button(buttonframe,text=" ADD MEDICINE ",
-                      font=("arial",12,"bold"),bg="darkred",fg="white")
+        button=Button(buttonframe,command=self.AddMedlower,text=" ADD MEDICINE ",
+                      font=("arial",12,"bold"),bg="darkgreen",fg="white")
         button.grid(row=0,column=0,padx=0,pady=5)
         button=Button(buttonframe,text="  UPDATE  ",
-                      font=("arial",12,"bold"),bg="darkred",fg="white")
+                      font=("arial",12,"bold"),bg="orange",fg="white")
         button.grid(row=0,column=1,padx=2,pady=5)
         button=Button(buttonframe,text="  DELETE  ",
                       font=("arial",12,"bold"),bg="darkred",fg="white")
         button.grid(row=0,column=2,padx=3,pady=5)
         button=Button(buttonframe,text="  RESET  ",
-                      font=("arial",12,"bold"),bg="darkred",fg="white")
+                      font=("arial",12,"bold"),bg="purple",fg="white")
         button.grid(row=0,column=3,padx=4,pady=5)
         button=Button(buttonframe,text="  EXIT  ",
-                      font=("arial",12,"bold"),bg="darkred",fg="white")
+                      font=("arial",12,"bold"),bg="darkgrey",fg="white")
         button.grid(row=0,column=4,padx=5,pady=5)
              #SEARCH BY OPTION:-
         Iblsearch=Label(buttonframe,font=("ariel",17,"bold"),
                         text="  SEARCH BY  ",
-                        padx=2,bg="darkred",fg="white",)
+                        padx=2,bg="darkblue",fg="white",)
         Iblsearch.grid(row=0,column=5,padx=6,pady=5,sticky=W)
         serch_combo=ttk.Combobox(buttonframe,width=12,font=("arial",13,"bold"),state="readonly")
         serch_combo["value"]=("Ref","Lot","Medname")
@@ -72,80 +86,80 @@ class ummtracker:
         txtserch=Entry(buttonframe,bd=3,relief=RIDGE,width=12,font=("ariel",12,"bold"))
         txtserch.grid(row=0,column=8,padx=8,pady=5)
         button=Button(buttonframe,text=" SEARCH  ",
-                      font=("arial",12,"bold"),bg="darkred",fg="white")
+                      font=("arial",12,"bold"),bg="darkblue",fg="white")
         button.grid(row=0,column=9,padx=9,pady=5)
         button=Button(buttonframe,text="  SHOW ALL  ",
-                      font=("arial",12,"bold"),bg="darkred",fg="white")
+                      font=("arial",12,"bold"),bg="black",fg="white")
         button.grid(row=0,column=10,padx=10,pady=5)
         #*****************************************************label and entry:-************************************************************
         refno=Label(dataframeleft, font=("arial", 10, "bold"), text="REFERENCE NO", padx=0)
         refno.grid(row=0,column=0,sticky=W)
         #******************************FETCHING DATA FROM SMALL WINDOW ON RIGHT SIDE TO LEFT COMBO BOXES LIKE REF,MEDICINE NAME***************************************
        
-        conn=mysql.connector.connect(host="localhost",user="root",password="**********",database="UMMTRACKER")
+        conn=mysql.connector.connect(host="localhost",user="root",password="7006760858",database="UMMTRACKER")
         cursor=conn.cursor()
         cursor.execute("SELECT REF FROM PHARMA")
         row=cursor.fetchall()
-        ref_combo=ttk.Combobox(dataframeleft, width=20, font=("arial", 12, "bold"), state="readonly")
+        ref_combo=ttk.Combobox(dataframeleft,textvariable=self.refvar,width=20, font=("arial", 12, "bold"), state="readonly")
         ref_combo["values"]=row
         ref_combo.current(0)
         ref_combo.grid(row=0,column=1)
         refno=Label(dataframeleft, font=("arial", 10, "bold"), text="COMPANY NAME", padx=2)
         refno.grid(row=1,column=0,sticky=W)
-        ref_combo=Entry(dataframeleft, width=22, font=("arial", 12, "bold"))
+        ref_combo=Entry(dataframeleft,  textvariable=self.cpynamevar,width=22, font=("arial", 12, "bold"))
         ref_combo.grid(row=1,column=1)
         refno=Label(dataframeleft, font=("arial", 10, "bold"), text="TYPE OF MEDICINE", padx=2)
         refno.grid(row=2,column=0,sticky=W)
-        ref_combo=ttk.Combobox(dataframeleft, width=20, font=("arial", 12, "bold"), state="readonly")
+        ref_combo=ttk.Combobox(dataframeleft,textvariable=self.typevar ,width=20, font=("arial", 12, "bold"), state="readonly")
         ref_combo["values"]=("TABLET", "LIQUID", "CAPSULE","DROPS","INJECTION","INHALER")
         ref_combo.current(0)
         ref_combo.grid(row=2,column=1)
         refno=Label(dataframeleft, font=("arial", 10, "bold"), text="MEDICINE NAME", padx=2)
         refno.grid(row=3,column=0,sticky=W)
         #******************************FETCHING DATA FROM SMALL WINDOW ON RIGHT SIDE TO LEFT COMBO BOXES LIKE REF,MEDICINE TYPE,MEDICINE NAME***************************************
-        conn=mysql.connector.connect(host="localhost",user="root",password="**********",database="UMMTRACKER")
+        conn=mysql.connector.connect(host="localhost",user="root",password="7006760858",database="UMMTRACKER")
         cursor=conn.cursor()
         cursor.execute("SELECT MEDNAME FROM PHARMA")
         row=cursor.fetchall()
-        ref_combo=ttk.Combobox(dataframeleft, width=20, font=("arial", 12, "bold"), state="readonly")
+        ref_combo=ttk.Combobox(dataframeleft,  textvariable=self.mednamevar,width=20, font=("arial", 12, "bold"), state="readonly")
         ref_combo["values"]=row
         ref_combo.current(0)
         ref_combo.grid(row=3,column=1)
         refno=Label(dataframeleft, font=("arial", 10, "bold"), text="LOT NO", padx=2)
         refno.grid(row=4,column=0,sticky=W)
-        ref_combo=Entry(dataframeleft,font=("arial", 12, "bold"),background="white",width=22)
+        ref_combo=Entry(dataframeleft, textvariable=self.lotvar,font=("arial", 12, "bold"),background="white",width=22)
         ref_combo.grid(row=4,column=1)
         refno=Label(dataframeleft, font=("arial", 10, "bold"), text="ISSUE DATE", padx=2)
         refno.grid(row=5,column=0,sticky=W)
-        ref_combo=Entry(dataframeleft, width=22, font=("arial", 12, "bold"))
+        ref_combo=Entry(dataframeleft,  textvariable=self.issuevar,width=22, font=("arial", 12, "bold"))
         ref_combo.grid(row=5,column=1)
         refno=Label(dataframeleft, font=("arial", 10, "bold"), text="EXP DATE", padx=2)
         refno.grid(row=6,column=0,sticky=W)
-        ref_combo=Entry(dataframeleft, width=22, font=("arial", 12, "bold"))
+        ref_combo=Entry(dataframeleft,  textvariable=self.expirevar,width=22, font=("arial", 12, "bold"))
         ref_combo.grid(row=6,column=1)
         refno=Label(dataframeleft, font=("arial", 10, "bold"), text="USES", padx=2)
         refno.grid(row=7,column=0,sticky=W)
-        ref_combo=Entry(dataframeleft, width=22, font=("arial", 12, "bold"))
+        ref_combo=Entry(dataframeleft,  textvariable=self.usesvar,width=22, font=("arial", 12, "bold"))
         ref_combo.grid(row=7,column=1)
         refno=Label(dataframeleft, font=("arial", 10, "bold"), text="SIDE EFFECT", padx=2)
         refno.grid(row=8,column=0,sticky=W)
-        ref_combo=Entry(dataframeleft, width=22, font=("arial", 12, "bold"))
+        ref_combo=Entry(dataframeleft,  textvariable=self.efectsvar,width=22, font=("arial", 12, "bold"))
         ref_combo.grid(row=8,column=1)
         refno=Label(dataframeleft, font=("arial", 10, "bold"), text="WARNINGS", padx=2)
         refno.grid(row=9,column=0,sticky=W)
-        ref_combo=Entry(dataframeleft, width=22, font=("arial", 12, "bold"))
+        ref_combo=Entry(dataframeleft,  textvariable=self.warningvar,width=22, font=("arial", 12, "bold"))
         ref_combo.grid(row=9,column=1)
         refno=Label(dataframeleft, font=("arial", 10, "bold"), text="DOSAGE", padx=2)
         refno.grid(row=10,column=0,sticky=W)
-        ref_combo=Entry(dataframeleft, width=22, font=("arial", 12, "bold"))
+        ref_combo=Entry(dataframeleft, textvariable=self.dosagevar, width=22, font=("arial", 12, "bold"))
         ref_combo.grid(row=10,column=1)
         refno=Label(dataframeleft, font=("arial", 10, "bold"), text="MED PRICE", padx=2)
         refno.grid(row=0,column=2,sticky=W)
-        ref_combo=Entry(dataframeleft, width=22, font=("arial", 12, "bold"))
+        ref_combo=Entry(dataframeleft, textvariable=self.pricevar, width=22, font=("arial", 12, "bold"))
         ref_combo.grid(row=0,column=3)
         refno=Label(dataframeleft, font=("arial", 10, "bold"), text="PRODUCT QTY")
         refno.grid(row=1,column=2,sticky=W)
-        ref_combo=Entry(dataframeleft, width=22, font=("arial", 12, "bold"))
+        ref_combo=Entry(dataframeleft, textvariable=self.qntyvar, width=22, font=("arial", 12, "bold"))
         ref_combo.grid(row=1,column=3,sticky=W)
         #************************************xuv auer jaha auer tag********************************************
         refno=Label(dataframeleft,font=("arial", 18, "bold"), text="ZUV AUER---JAHA AUER", padx=2,pady=6,background="white",foreground="darkblue")
@@ -172,11 +186,11 @@ class ummtracker:
         self.imagelogo6=ImageTk.PhotoImage(logo6)
         button2=Button(self.root,image=self.imagelogo6,borderwidth=0)        #position of image
         button2.place(x=1150,y=160)
-        refno=Label(dataframeright,font=("arial", 10, "bold"), text="REFERENCE NO",)
+        refno=Label(dataframeright,font=("arial", 10, "bold"), text="REFERENCE NO")
         refno.place(x=0,y=0)
         refno=Entry(dataframeright,textvariable=self.addrefvar,width=19, font=("arial", 12, "bold"),bg="white",bd=2,relief=RIDGE,)
         refno.place(x=115,y=0)
-        medname=Label(dataframeright,font=("arial", 10, "bold"), text="MEDICINE NAME",)
+        medname=Label(dataframeright,font=("arial", 10, "bold"), text="MEDICINE NAME")
         medname.place(x=0,y=25)
         medname=Entry(dataframeright,textvariable=self.addmedvar,width=19, font=("arial", 12, "bold"),bg="white",bd=2,relief=RIDGE,)
         medname.place(x=115,y=25)
@@ -197,6 +211,7 @@ class ummtracker:
         self.medtable.column("REF",width=100)
         self.medtable.column("MEDNAME",width=100)
         self.medtable.bind("<ButtonRelease-1>",self.medget) #BINDING DATA FROM SMALL FRAME TO REF AND MEDNMAE
+        #*******************************MAKING OF ADDD,UPDATE,DELETE,CLEAR PORTION->RIGHTFRAME***********************
         downframe=Frame(dataframeright,bd=4,relief=RIDGE,bg="darkred")
         downframe.place(x=320,y=110,width=135,height=160)
         btnAddmed=Button(downframe,command=self.AddMed,text=" ADD  ",width=12,
@@ -262,22 +277,24 @@ class ummtracker:
                    #FIRSTLY CREATE DATABASE NAMELY UMMTRACKER AND THEN CREATE TWO TABLES IN THAT--->PHARMA AND 
                    #**********************************WORKING OF ADD BUTTON*****************************************
     def AddMed(self):
-        conn=mysql.connector.connect(host="localhost",user="root",password="**********",database="UMMTRACKER")
+        conn=mysql.connector.connect(host="localhost",user="root",password="7006760858",database="UMMTRACKER")
         cursor=conn.cursor()
+        val1=self.addrefvar.get()
+        val2=self.addmedvar.get()
         query="INSERT INTO PHARMA(Ref,Medname) VALUES (%s,%s)"
-        values=(self.addrefvar.get(),
-         self.addmedvar.get())
+        values=(val1,val2)
         cursor.execute(query,values)
         conn.commit()
+        messagebox.showinfo("Success","MEDICINE ADDED SUCCESSFULLY")
+        conn.close()
         self.fetchdataMed()
         self.medget()
-        conn.close()
         self.root.update_idletasks()
-        messagebox.showinfo("Success","MEDICINE ADDED SUCCESSFULLY")
+        
 
 #***************************SHOWING DATA ADDED IN RIGHT SIDE SMALL WINDOW********************************************
     def fetchdataMed(self):
-        conn=mysql.connector.connect(host="localhost",user="root",password="**********",database="UMMTRACKER")
+        conn=mysql.connector.connect(host="localhost",user="root",password="7006760858",database="UMMTRACKER")
         cursor=conn.cursor()
         cursor.execute("SELECT * FROM PHARMA")
         rows=cursor.fetchall()
@@ -298,19 +315,21 @@ class ummtracker:
             if self.addrefvar.get()=="" or self.addmedvar.get()=="":
                 messagebox.showerror("Error","BOTH FIELDS ARE REQUIRED")
             else:
-                 conn=mysql.connector.connect(host="localhost",user="root",password="**********",database="UMMTRACKER")
+                 conn=mysql.connector.connect(host="localhost",user="root",password="7006760858",database="UMMTRACKER")
                  cursor=conn.cursor()
                  cursor.execute("UPDATE PHARMA SET MEDNAME=%s WHERE REF=%s",(
                                                                self.addmedvar.get(),
                                                             self.addrefvar.get()
                  ))
                  conn.commit()
+                 print("updated successfully")
+                 messagebox.showinfo("Success","DATA UPDATED SUCCESSFULLY")
                  self.fetchdatamed()
                  conn.close()
-                 messagebox.showinfo("Success","DATA UPDATED SUCCESSFULLY")
+               
         #******************************************************DELETING MEDICINE ITEM**************************************
     def deletemed(self):
-        conn=mysql.connector.connect(host="localhost",user="root",password="**********",database="UMMTRACKER")
+        conn=mysql.connector.connect(host="localhost",user="root",password="7006760858",database="UMMTRACKER")
         cursor=conn.cursor()
         query="DELETE FROM PHARMA WHERE REF=%s"
         value=(self.addrefvar.get(),)
@@ -325,7 +344,37 @@ class ummtracker:
         self.addmedvar.set("")
         messagebox.showinfo("Success","DATA CLEARED SUCCESSFULLY")
 
+                #***********************************WORKING OF LOWER BUTTONS:************************************
+    
+    def AddMedlower(self):
+        if self.refvar.get()=="" or self.cpynamevar.get()=="":
+                messagebox.showerror("Error","BOTH FIELDS ARE REQUIRED")
+        else:
+          conn = mysql.connector.connect(host="localhost",user="root",password="7006760858",database="UMMTRACKER")
+          cursor = conn.cursor()
+          val1 = self.refvar.get()
+          val2 = self.cpynamevar.get()
+          val3 = self.typevar.get()
+          val4 = self.mednamevar.get()
+          val5 = self.lotvar.get() 
+          val6 = self.issuevar.get()  
+          val7 = self.expirevar.get()  
+          val8 = self.usesvar.get()
+          val9 = self.efectsvar.get()
+          val10 = self.warningvar.get()
+          val11 = self.dosagevar.get()
+          val12 = self.pricevar.get()
+          val13 = self.qntyvar.get()
+          query = """
+        INSERT INTO PHDATA (REFNO, COMPANYNAME, MEDICINETYPE, MEDICINENAME, LOTNO, ISSUEDAT, EXPDAT, USES, EFFECTS, WARNINGS, DOSAGE, MEDPRICE, QNTY)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
+          values = (val1, val2, val3, val4, val5, val6, val7, val8, val9, val10, val11, val12, val13)
 
+          cursor.execute(query, values)
+          conn.commit()
+          messagebox.showinfo("Success", "Data inserted successfully!")
+          conn.close()
 
 
 
@@ -335,4 +384,4 @@ if __name__ =="__main__":
     root=Tk()
     obj=ummtracker(root)
     root.mainloop() #CLOSING OF ROOT
-        
+         
